3831843c4b35725cb5e39c23ed5f48a70bb969d9
garth stevens <gstevens96@gmail.com>
2018-07-06 13:53:48 -0600

    changed the platform_id numbers in the games seed to reflect the actual consoles

--- seeds/02_games.js
+++ seeds/02_games.js
@@ -7,21 +7,21 @@ module.exports.seed = (knex, Promise) => {
           title: 'Doom', 
           dev: 'id Software', 
           physOrDigi: 'Physical', 
-          platform_id: 1 
+          platform_id: 23 
         },
         { 
           id: 2, 
           title: 'Payday2', 
           dev: 'Overkill Software', 
           physOrDigi: 'Physical', 
-          platform_id: 2
+          platform_id: 7
         },
         { 
           id: 3, 
           title: 'Killing Floor 2', 
           dev: 'Tripwire Studios', 
           physOrDigi: 'Digital', 
-          platform_id: 3 }
+          platform_id: 29 }
       ])
     .then(() => knex.raw('ALTER SEQUENCE games_id_seq RESTART WITH 4;'))
     })
